5fc17959713011884ce7023b53e3d62931f7e877
terminaldweller <devi@terminaldweller.com>
2024-11-04 14:06:58 -0500

    fixed the wrong condition

--- utils.go
+++ utils.go
@@ -3,7 +3,7 @@ package main
 import "github.com/lrstanley/girc"
 
 func IrcJoin(irc *girc.Client, channel []string) {
-	if len(channel) >= 0 && channel[0] == "" {
+	if len(channel) > 1 && channel[1] != "" {
 		irc.Cmd.JoinKey(channel[0], channel[1])
 	} else {
 		irc.Cmd.Join(channel[0])
